de98c9968bfa06655b80a55f2766966d97f32e99
PiGod <237793303@qq.com>
2024-07-21 16:19:15 +0800

    ph

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -5,7 +5,7 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#define NBUCKET 5
+#define NBUCKET 7
 #define NKEYS 100000
 
 struct entry {
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t locks[NBUCKET];
 
 double
 now()
@@ -52,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&locks[i]);       // acquire lock
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&locks[i]);     // release lock
   }
 
 }
@@ -117,7 +119,10 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  //初始化锁
+  for(int i = 0; i < NBUCKET; ++i) {
+      pthread_mutex_init(&locks[i], NULL);
+  }
   //
   // first the puts
   //
